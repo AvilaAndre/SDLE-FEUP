d70d0435c3cc97c700094c1a24c881e6a5e24050
AvilaAndre <avila_andre14@hotmail.com>
2023-11-09 23:51:01 +0000

    Add icon on local and shared lists

--- app/src/lib/components/ListCard.svelte
+++ app/src/lib/components/ListCard.svelte
@@ -1,12 +1,23 @@
 <script lang="ts">
+    import PublishIcon from "$lib/icons/PublishIcon.svelte";
+    import SavedLocallyIcon from "$lib/icons/SavedLocallyIcon.svelte";
+
     export let title: string;
     export let id: string;
+    export let shared: boolean = false;
 </script>
 
 <a
     href={"/list?id=" + id}
-    class="bg-white p-2 h-32 w-64 shadow-md rounded-md disabled card-1"
+    class="bg-white p-2 h-32 w-64 shadow-md rounded-md disabled card-1 relative"
 >
     <h4 class="text-xl">{title}</h4>
     <h4 class="text-xs text-slate-500">{id}</h4>
+    <span class="absolute right-1 bottom-1">
+        {#if shared}
+            <PublishIcon className="w-6" />
+        {:else}
+            <SavedLocallyIcon className="w-6" />
+        {/if}
+    </span>
 </a>
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -5,15 +5,40 @@
     type List = {
         title: string;
         id: string;
+        shared: boolean;
     };
 
     const lists: List[] = [
-        { title: "List1", id: "99e856ff-fa4e-4269-b087-fbf91a226f3c" },
-        { title: "List2", id: "671f3ff3-748b-4016-afc1-b6d4a0bdfab1" },
-        { title: "List3", id: "6b18023b-ed3f-4b93-808e-966b2d8dfb47" },
-        { title: "List4", id: "d977fc87-e5c1-4b06-bc21-42b553ebf5d0" },
-        { title: "List5", id: "bff4b478-c9f2-44db-b8fa-402302810bf9" },
-        { title: "List6", id: "916859a7-596d-4192-b132-f7c37d1d8bd0" },
+        {
+            title: "List1",
+            id: "99e856ff-fa4e-4269-b087-fbf91a226f3c",
+            shared: true,
+        },
+        {
+            title: "List2",
+            id: "671f3ff3-748b-4016-afc1-b6d4a0bdfab1",
+            shared: true,
+        },
+        {
+            title: "List3",
+            id: "6b18023b-ed3f-4b93-808e-966b2d8dfb47",
+            shared: false,
+        },
+        {
+            title: "List4",
+            id: "d977fc87-e5c1-4b06-bc21-42b553ebf5d0",
+            shared: false,
+        },
+        {
+            title: "List5",
+            id: "bff4b478-c9f2-44db-b8fa-402302810bf9",
+            shared: false,
+        },
+        {
+            title: "List6",
+            id: "916859a7-596d-4192-b132-f7c37d1d8bd0",
+            shared: true,
+        },
     ];
 </script>
 
@@ -27,7 +52,7 @@
     <div>New List Join List</div>
     <div class="grid grid-cols-3 gap-4">
         {#each lists as list}
-            <ListCard title={list.title} id={list.id} />
+            <ListCard title={list.title} id={list.id} shared={list.shared} />
         {/each}
     </div>
 </section>
