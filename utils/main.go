e50eeda3dc71b4ba48804750a295842f4dc1a6e6
AvilaAndre <avila_andre14@hotmail.com>
2023-12-02 19:30:56 +0000

    Coordinator Write

--- utils/main.go
+++ utils/main.go
@@ -18,3 +18,30 @@ func Int64ToString(number int64) string {
 func Float64ToString(number float64) string {
 	return fmt.Sprintf("%f", number)
 }
+
+type Stack[T any] struct {
+	values []T
+}
+
+func (s *Stack[T]) New() {
+	s.values = make([]T, 0)
+}
+
+func (s *Stack[T]) Push(newValue T) {
+	s.values = append(s.values, newValue)
+}
+
+func (s *Stack[T]) Pop() T {
+	var value T = s.values[len(s.values)-1]
+	s.values = s.values[:len(s.values)-1]
+
+	return value
+}
+
+func (s *Stack[T]) Peek() T {
+	return s.values[len(s.values)-1]
+}
+
+func (s *Stack[T]) Size() int {
+	return len(s.values)
+}
